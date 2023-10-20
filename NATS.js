9dba011203feb7a5b950b9ce6f7e4e9b41d354dd
root <root@2022Comp>
2023-10-20 11:34:22 -0400

    Able to connect to NATS

--- NATS.js
+++ NATS.js
@@ -2,12 +2,28 @@
 import nats from 'nats';
 
 // Connect to the NATS server
-const nc = await nats.connect({url: 'nats://127.0.0.1:4222:4222'});
+const nc = await nats.connect({
+  servers: 'nats://nats:4222', // Use the service name 'nats' defined in your Docker Compose file
+});
 
 nc.subscribe("cluster.*.schedule", {
   callback: (err, msg) => {
     console.log(err, msg)
   }
 })
+
+console.log('hello world')
+
+const message = {
+  cluster: "plane.test",
+  max_idle_secs: 30,
+  metadata: {},
+  executable: {
+      image: "ghcr.io/drifting-in-space/demo-image-drop-four",
+      env: {},
+  }
+}
+
+nc.publish('cluster.plane_test.schedule', message);
 
 export default nc
