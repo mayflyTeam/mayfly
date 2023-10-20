dfeb7d7060ea3f97e329172ce5201cd59d09bea8
root <root@2022Comp>
2023-10-20 14:14:06 -0400

    Able to get backend_id response back

--- NATS.js
+++ NATS.js
@@ -3,14 +3,7 @@ import nats from 'nats';
 
 // Connect to the NATS server
 const nc = await nats.connect({
-  servers: 'nats://nats:4222', // Use the service name 'nats' defined in your Docker Compose file
+  servers: 'http://localhost:4222',
 });
-
-nc.subscribe("cluster.*.schedule", {
-  callback: (err, msg) => {
-    console.log('Error: ', err)
-    console.log('Message: ', msg)
-  }
-})
 
 export default nc
--- api.js
+++ api.js
@@ -4,11 +4,11 @@ import nc from './NATS.js'
 
 const app = express();
 
-app.get('/', (req, res) => {
+app.get('/', async (req, res) => {
   // Get message from request body
   const message = JSON.stringify({
     cluster: "plane.test",
-    max_idle_secs: 30,
+    max_idle_secs: 60,
     metadata: {},
     executable: {
         image: "ghcr.io/drifting-in-space/demo-image-drop-four",
@@ -17,9 +17,15 @@ app.get('/', (req, res) => {
   })
 
   // Publish to NATS
-  nc.publish('cluster.plane_test.schedule', message);
+  const reply = await nc.request('cluster.plane_test.schedule', message, {max:1});
 
-  res.send('Published message');
+  const responseData = JSON.parse(reply.data);
+
+  console.log('Received NATS response:', responseData);
+
+  const url = `http://localhost:8080/_plane_backend=${responseData.Scheduled.backend_id}/`
+
+  res.send(url);
 });
 
 app.listen(3001, '0.0.0.0', () => {
