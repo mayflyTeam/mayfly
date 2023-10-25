3ee7cf190d297ca151440d04cc131f57674d2c3f
root <root@2022Comp>
2023-10-25 15:53:38 -0400

    DNS working for taishan.website

--- api.js
+++ api.js
@@ -7,23 +7,23 @@ const app = express();
 app.get('/', async (req, res) => {
   // Get message from request body
   const message = JSON.stringify({
-    cluster: "plane.test",
+    cluster: "taishan.website",
     max_idle_secs: 60,
     metadata: {},
     executable: {
-        image: "ghcr.io/drifting-in-space/demo-image-drop-four",
+        image: "ghcr.io/drifting-in-space/jamsocket-jupyter-notebook:sha-fa92787",
         env: {},
     }
   })
 
   // Publish to NATS
-  const reply = await nc.request('cluster.plane_test.schedule', message, {max:1});
+  const reply = await nc.request('cluster.taishan_website.schedule', message, {max:1});
 
   const responseData = JSON.parse(reply.data);
 
   console.log('Received NATS response:', responseData);
 
-  const url = `http://localhost:8080/_plane_backend=${responseData.Scheduled.backend_id}/`
+  const url = `http://${responseData.Scheduled.backend_id}.taishan.website:8080/`
 
   res.send(url);
 });
